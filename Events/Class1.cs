9df22931407bb55affc676d6719d76b8726aed18
iLighty33 <iLighty33@gmail.com>
2022-09-09 21:09:59 +0500

    Добавьте файлы проекта.

---
+++ Events/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events {
+
+    class Acc {
+
+        delegate void AccHandler(string sender);
+
+        event AccHandler? Notify;
+
+        public Acc(int sum) {
+            Sum = sum;
+        }
+        public int Sum { get; set; }
+
+        public void Put(int sum) {
+            Sum += sum;
+            Notify?.Invoke($"На счет поступило: {sum}");
+        }
+
+        public void Take(int sum) {
+
+            if (Sum >= sum) {
+                Sum -= sum;
+                Notify?.Invoke($"Со счёта снято:  {sum}");
+            }
+            else
+                Notify?.Invoke($"Недостаточно средств  {sum}");
+
+        }
+    }
+
+}    
+
+
---
+++ Events/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Events;
+Acc account = new(100);
+
+account.Put(20);
+
+Console.WriteLine($"Сумма на счете: {account.Sum}");
+
+account.Take(70);
+
+Console.WriteLine($"Сумма на счете: {account.Sum}");
+
+account.Take(180);
+
+Console.WriteLine($"Сумма на счете: {account.Sum}");
